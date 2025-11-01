c68f256893b6559b9c043d9b907a5b77a204a95e
edi0328 <edisonthai0328@gmail.com>
2025-11-01 13:27:26 -0400

    Add back home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         <>
             <div className="home-container">
                 <h3 className="home-welcome-message">Welcome! Improve your knowledge on courses ahead of your exams...  </h3>
-                <Link to="/generate" className="home-create-link">
+                <Link to="/create" className="home-create-link">
                     <button className="home-create-button">Create</button>
                 </Link>
                 <img src={exampleCheatsheet} alt="Example Cheat Sheet" className="home-example-image"/>
